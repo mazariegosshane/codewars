6cb0ab01ef9f4f0a8adeb8256f3856c7626461d9
Shane <mazariegosshane@gmail.com>
2025-09-11 16:57:31 -0700

    added new codewars challenge

---
+++ count-by-x.js
@@ -0,0 +1,26 @@
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// x = 1, n = 10 --> [1,2,3,4,5,6,7,8,9,10]
+// x = 2, n = 5  --> [2,4,6,8,10]
+
+
+// my solution
+// didnt solve
+
+
+// other solution
+function countBy(x, n) {
+    var z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
+
+// i has to start with 1, not 0 
+// each iteration we push to 'z' array the number(x) and it will loop thru (n) number of times
